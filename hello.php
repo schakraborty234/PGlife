d4aab578d49605967b4f25a2e905d8f9d7266214
schakraborty234 <139431278+schakraborty234@users.noreply.github.com>
2023-08-20 12:59:10 +0530

    Add files via upload

---
+++ hello.php
@@ -0,0 +1,36 @@
+
+<?php
+
+// $a=20;
+// $b=10;
+// $operator= "*";
+
+// switch($operator){
+// case "+";
+// echo $a+$b;
+// break;
+// case "-";
+// echo $a-$b;
+// break;
+// case "*";
+// echo $a*$b;
+// break;
+// case "/";
+// echo $a/$b;
+// break;
+ 
+// default:
+// echo "this operator is not supported";
+// }
+
+// $arr = array(2, 4,6,8,10);
+// foreach($arr as $num){
+//     echo $num. " ";
+// }
+
+
+$arr= array("George","Barack","Donald");
+foreach($arr as $president){
+    echo $president. " ";
+}
+?>
