f8ea7305ccad9624bae8a8ce5fdf9514adbdfb96
schakraborty234 <139431278+schakraborty234@users.noreply.github.com>
2023-08-20 12:53:15 +0530

    Add files via upload

---
+++ react-app/src/NoProperty.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const NoProperty = () => {
+
+  return (
+    <div className="no-property-container">
+      <p>No PG to list</p>
+    </div>
+  );
+}
+
+export default NoProperty;
